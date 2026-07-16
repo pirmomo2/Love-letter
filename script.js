f7e229fbeb5b55698278cab13e2d8d77e8c428ac
Pirmomo2 <mamanma2021@gmail.com>
2026-07-16 05:22:29 +0300

    Added a function for the "Yes" button

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const noButton = document.querySelector(".no");
-	if (!noButton) {
+	const No = document.querySelector(".no");
+	const Yes = document.querySelector(".yes");
+	if (!No) {
 		return;
 	}
-	const moveNoButton = () => {
-		const buttonWidth = noButton.offsetWidth;
-		const buttonHeight = noButton.offsetHeight;
+	const yesceleb = () => {
+		const card = document.getElementById('card');
+		const title = document.createElement('p');
+		title.textContent = 'Thank you so much! We will now be in love forever and ever!';
+		title.classList.add('request');
+
+		card.replaceChildren(title);
+	}
+	const moveno = () => {
+		const buttonWidth = No.offsetWidth;
+		const buttonHeight = No.offsetHeight;
 		const maxX = Math.max(0, window.innerWidth - buttonWidth - 20);
 		const maxY = Math.max(0, window.innerHeight - buttonHeight - 20);
 		const randomX = Math.floor(Math.random() * maxX);
 		const randomY = Math.floor(Math.random() * maxY);
-		noButton.style.position = "fixed";
-		noButton.style.left = `${randomX}px`;
-		noButton.style.top = `${randomY}px`;
+		No.style.position = "fixed";
+		No.style.left = `${randomX}px`;
+		No.style.top = `${randomY}px`;
 	};
-	noButton.addEventListener("mouseenter", moveNoButton);
+	Yes.addEventListener("click", yesceleb);
+	No.addEventListener("mouseenter", moveno);
 });
